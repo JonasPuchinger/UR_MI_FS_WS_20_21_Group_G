c7ef5bf88c47484cc6699cb28b3985e119c5f23a
JonasPuchinger <j.g.puchinger@kabelmail.net>
2021-01-14 14:05:33 +0100

    Added verified status

--- analysis/count_tweets.py
+++ analysis/count_tweets.py
@@ -81,12 +81,21 @@ def get_followers(screen_name, user_id):
         return followers
     return 0
 
+def get_verification_status(screen_name, user_id):
+    f_path = os.path.join(USERS_SOURCE_FOLDER, f'{screen_name}.json')
+    if os.path.isfile(f_path):
+        with open(f_path, 'r', encoding='utf-8') as infile:
+            verified = str(next((u['raw_data']['verified'] for u in json.load(infile) if u['id_'] == str(user_id)), 'False'))
+        return verified
+    return 'False'
+
 with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
     for p in json.load(infile):
         p_name = p['Name']
         p_screen_name = p['screen_name']
         p_user_id = p['id']
         p_followers = get_followers(screen_name=p_screen_name, user_id=p_user_id)
+        p_verified = get_verification_status(screen_name=p_screen_name, user_id=p_user_id)
         p_party = p['Partei']
         p_total_tweets_found, p_tweets_by_politician = count_tweets_for_user(screen_name=p_screen_name, user_id=p_user_id)
         p_ratio_own_tweets = round((float(p_tweets_by_politician) / float(p_total_tweets_found)) * 100, 2) if p_total_tweets_found != 0 else 0
@@ -101,6 +110,7 @@ with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
             'name': p_name,
             'screen_name': p_screen_name,
             'followers': p_followers,
+            'verified': p_verified,
             'party': p_party,
             'total_tweets_found': p_total_tweets_found,
             'tweets_by_politician': p_tweets_by_politician,
