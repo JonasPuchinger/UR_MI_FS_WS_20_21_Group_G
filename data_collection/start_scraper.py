4dad4e4ae9abf41744646ee693ee7718556d29ba
Jonas Puchinger <j.g.puchinger@kabelmail.net>
2021-01-21 18:02:31 +0100

    Deleted formated_data and removed unnecessary imports

--- data_collection/start_scraper.py
+++ data_collection/start_scraper.py
@@ -1,13 +1,10 @@
 import sys
 sys.path.append('../TweetScraper')
 sys.path.append('../analysis')
+import os
 import configparser
 import datetime
 import json
-from timeit import default_timer as timer
-from scrapy.crawler import CrawlerProcess, CrawlerRunner
-from scrapy.utils.project import get_project_settings
-from twisted.internet import defer, reactor
 from generate_requests import twitter_requests
 from TweetScraper_API import start_tweet_scraper
 from combine_files import combine_files
@@ -31,8 +28,13 @@ politicians_requests = twitter_requests(screen_names=POLITICIANS_SCREEN_NAMES, s
 
 start_tweet_scraper(requests=politicians_requests)
 
+RAW_DATA_TWEETS = '../Data/tweet/'
+RAW_DATA_USERS = '../Data/user/'
+TWEET_SAVE_DIR = '../analysis/formated_data_2/tweet/'
+USER_SAVE_DIR = '../analysis/formated_data_2/user/'
+
 # Combine tweets
-combine_files(source_folder='../Data/tweet/', results_folder='../analysis/formated_data_2/tweet/')
+combine_files(source_folder=RAW_DATA_TWEETS, results_folder=TWEET_SAVE_DIR)
 
 # Combine users
-combine_files(source_folder='../Data/user/', results_folder='../analysis/formated_data_2/user/')
+combine_files(source_folder=RAW_DATA_USERS, results_folder=USER_SAVE_DIR)
