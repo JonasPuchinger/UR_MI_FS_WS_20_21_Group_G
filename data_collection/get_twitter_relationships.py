c7fd64fffdb30930651b90c11746cb5c35d89b85
JonasPuchinger <j.g.puchinger@kabelmail.net>
2021-02-05 18:42:07 +0100

    Added new data

--- data_collection/get_twitter_relationships.py
+++ data_collection/get_twitter_relationships.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import json
 from TwitterFollowChecker import TwitterFollowChecker
 import configparser
+from selenium.common.exceptions import TimeoutException
 
 # Constants
 RESULTS_FOLDER_POLITICIANS = '../analysis/formated_data/relationship_politicians/'
@@ -39,8 +41,8 @@ twitter_username = config['twitter_account_credentials']['twitter_username']
 twitter_handle = config['twitter_account_credentials']['twitter_handle']
 twitter_pw = config['twitter_account_credentials']['twitter_pw']
 
-# Get and save relations for all politicians
-for p in [pol for pol in all_politicians if f'{pol["screen_name"]}.json' not in os.listdir(RESULTS_FOLDER_POLITICIANS) and pol["screen_name"] not in ['Wellenreuther', 'ChristophFDP']]:
+# Get and save relations for mdbs
+for p in [pol for pol in mdbs if f'{pol["screen_name"]}.json' not in os.listdir(RESULTS_FOLDER_POLITICIANS)]:
     p_screen_name = p['screen_name']
     p_relationships = []
     # Save mdb relations
@@ -135,6 +137,77 @@ for p in [pol for pol in all_politicians if f'{pol["screen_name"]}.json' not in 
         json.dump(p_relationships, outfile, ensure_ascii=False)
 
 
+# Get and save relations for additional politicians
+for add_pol in [acc for acc in additional_politicians if f'{acc["screen_name"]}.json' not in os.listdir(RESULTS_FOLDER_POLITICIANS)]:
+    add_pol_screen_name = add_pol['screen_name']
+    add_pol_relationships = []
+    if f'{add_pol_screen_name}_temp.json' in os.listdir(RESULTS_FOLDER_POLITICIANS):
+        with open(f'{RESULTS_FOLDER_POLITICIANS}/{add_pol_screen_name}_temp.json', 'r', encoding='utf-8') as temp_infile:
+            add_pol_relationships = json.load(temp_infile)
+    done_rels = [r['target_screen_name'] for r in add_pol_relationships]
+    print(len(done_rels))
+    # Create TwitterFollowChecker
+    follow_checker = TwitterFollowChecker(twitter_username=twitter_username, twitter_handle=twitter_handle, twitter_pw=twitter_pw)
+    try:
+        for pol in [p for p in all_politicians if p['screen_name'] != add_pol_screen_name and p['screen_name'] not in done_rels]:
+            print(add_pol_screen_name, pol['screen_name'])
+            cur_rel = follow_checker.get_follow(add_pol_screen_name, pol['screen_name'])
+            if cur_rel[0] == True:
+                if cur_rel[0] == True:
+                    add_pol_relationships.append({
+                        'target_screen_name': pol['screen_name'],
+                        'target_id': pol['id'],
+                        'value': 2
+                    })
+                elif cur_rel[0] == False:
+                    add_pol_relationships.append({
+                        'target_screen_name': pol['screen_name'],
+                        'target_id': pol['id'],
+                        'value': 1
+                    })
+            elif cur_rel[0] == False:
+                add_pol_relationships.append({
+                        'target_screen_name': pol['screen_name'],
+                        'target_id': pol['id'],
+                        'value': 0
+                    })
+        
+        print('---\nAll politicians done\n---')
+        
+        for aa in [a for a in additional_accounts if a['screen_name'] not in done_rels]:
+            print(add_pol_screen_name, aa['screen_name'])
+            rel_aa = follow_checker.get_follow(add_pol_screen_name, aa['screen_name'])
+            if rel_aa[0] == True:
+                if rel_aa[0] == True:
+                    add_pol_relationships.append({
+                        'target_screen_name': aa['screen_name'],
+                        'target_id': aa['id'],
+                        'value': 2
+                    })
+                elif rel_aa[0] == False:
+                    add_pol_relationships.append({
+                        'target_screen_name': aa['screen_name'],
+                        'target_id': aa['id'],
+                        'value': 1
+                    })
+            elif rel_aa[0] == False:
+                add_pol_relationships.append({
+                        'target_screen_name': aa['screen_name'],
+                        'target_id': aa['id'],
+                        'value': 0
+                    })
+        
+    except TimeoutException:
+        with open(os.path.join(RESULTS_FOLDER_POLITICIANS, f'{add_pol_screen_name}_temp.json'), 'w+', encoding='utf-8') as temp_outfile:
+            json.dump(add_pol_relationships, temp_outfile, ensure_ascii=False)
+        print('Timeout')
+        sys.exit()
+
+    with open(os.path.join(RESULTS_FOLDER_POLITICIANS, f'{add_pol_screen_name}.json'), 'w+', encoding='utf-8') as outfile:
+        json.dump(add_pol_relationships, outfile, ensure_ascii=False)
+        os.remove(os.path.join(RESULTS_FOLDER_POLITICIANS, f'{add_pol_screen_name}_temp.json'))
+
+
 # Create folder if it does not already exist
 if not os.path.exists(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS):
     os.makedirs(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS)
@@ -143,30 +216,68 @@ if not os.path.exists(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS):
 for add_acc in [acc for acc in additional_accounts if f'{acc["screen_name"]}.json' not in os.listdir(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS)]:
     aa_screen_name = add_acc['screen_name']
     aa_relationships = []
+    if f'{aa_screen_name}_temp.json' in os.listdir(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS):
+        with open(f'{RESULTS_FOLDER_ADDITIONAL_ACCOUNTS}/{aa_screen_name}_temp.json', 'r', encoding='utf-8') as temp_infile:
+            aa_relationships = json.load(temp_infile)
+    done_rels = [r['target_screen_name'] for r in aa_relationships]
+    print(len(done_rels))
     # Create TwitterFollowChecker
     follow_checker = TwitterFollowChecker(twitter_username=twitter_username, twitter_handle=twitter_handle, twitter_pw=twitter_pw)
-    for pol in all_politicians:
-        print(aa_screen_name, pol['screen_name'])
-        cur_rel = follow_checker.get_follow(aa_screen_name, pol['screen_name'])
-        if cur_rel[0] == True:
+    try:
+        for pol in [p for p in all_politicians if p['screen_name'] not in done_rels]:
+            print(aa_screen_name, pol['screen_name'])
+            cur_rel = follow_checker.get_follow(aa_screen_name, pol['screen_name'])
             if cur_rel[0] == True:
-                aa_relationships.append({
-                    'target_screen_name': pol['screen_name'],
-                    'target_id': pol['id'],
-                    'value': 2
-                })
+                if cur_rel[0] == True:
+                    aa_relationships.append({
+                        'target_screen_name': pol['screen_name'],
+                        'target_id': pol['id'],
+                        'value': 2
+                    })
+                elif cur_rel[0] == False:
+                    aa_relationships.append({
+                        'target_screen_name': pol['screen_name'],
+                        'target_id': pol['id'],
+                        'value': 1
+                    })
             elif cur_rel[0] == False:
                 aa_relationships.append({
-                    'target_screen_name': pol['screen_name'],
-                    'target_id': pol['id'],
-                    'value': 1
-                })
-        elif cur_rel[0] == False:
-            aa_relationships.append({
-                    'target_screen_name': pol['screen_name'],
-                    'target_id': pol['id'],
-                    'value': 0
-                })
+                        'target_screen_name': pol['screen_name'],
+                        'target_id': pol['id'],
+                        'value': 0
+                    })
+
+        print('---\nAll politicians done\n---')
+
+        for aa in [a for a in additional_accounts if a['screen_name'] != aa_screen_name and a['screen_name'] not in done_rels]:
+            print(aa_screen_name, aa['screen_name'])
+            rel_aa = follow_checker.get_follow(aa_screen_name, aa['screen_name'])
+            if rel_aa[0] == True:
+                if rel_aa[0] == True:
+                    aa_relationships.append({
+                        'target_screen_name': aa['screen_name'],
+                        'target_id': aa['id'],
+                        'value': 2
+                    })
+                elif rel_aa[0] == False:
+                    aa_relationships.append({
+                        'target_screen_name': aa['screen_name'],
+                        'target_id': aa['id'],
+                        'value': 1
+                    })
+            elif rel_aa[0] == False:
+                aa_relationships.append({
+                        'target_screen_name': aa['screen_name'],
+                        'target_id': aa['id'],
+                        'value': 0
+                    })
+
+    except TimeoutException:
+        with open(os.path.join(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS, f'{aa_screen_name}_temp.json'), 'w+', encoding='utf-8') as temp_outfile:
+            json.dump(aa_relationships, temp_outfile, ensure_ascii=False)
+        print('Timeout')
+        sys.exit()
 
     with open(os.path.join(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS, f'{aa_screen_name}.json'), 'w+', encoding='utf-8') as outfile:
         json.dump(aa_relationships, outfile, ensure_ascii=False)
+        os.remove(os.path.join(RESULTS_FOLDER_ADDITIONAL_ACCOUNTS, f'{aa_screen_name}_temp.json'))
