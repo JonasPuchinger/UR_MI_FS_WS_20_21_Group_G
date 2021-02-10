4cfd459833a696ec73733c0a5878c20ea605b2f5
TheresaStrohmeier <strohmeier.theresa@web.de>
2021-02-10 09:47:56 +0100

    Clean Code

--- analysis/covid_tweets_analysis/get_covid_tweets.py
+++ analysis/covid_tweets_analysis/get_covid_tweets.py
@@ -8,30 +8,29 @@ from nltk.corpus import stopwords
 import codecs
 from fuzzywuzzy import fuzz
 from nltk.tokenize import TweetTokenizer
-import shutil
 
-POLITICIANS_LIST = '../../assets/all_politicians.json'
+POLITICIANS_LIST = '../../assets/test_politicians.json'
 TWEETS_SOURCE_FOLDER = '../formated_data/tweet/'
-COVID_TWEETS_SOURCE_FOLDER = '../filtered_data/covid_tweets/'
-NON_COVID_TWEETS_SOURCE_FOLDER = '../filtered_data/non_covid_tweets/'
+COVID_TWEETS_FOLDER = '../filtered_data/covid_tweets/'
+NON_COVID_TWEETS_FOLDER = '../filtered_data/non_covid_tweets/'
 RESULTS_FILE_OVERVIEW = 'covid_tweets_overview.csv'
 RESULTS_FILE_DETAIL = 'covid_tweets_detail.csv'
 PREPROCESSED_WORDLIST_FUZ = '../preprocessing_wordlist/wordlist_fuzzy.json'
 PREPROCESSED_WORDLIST = '../preprocessing_wordlist/wordlist_matching.json'
 UNCERTAIN_WORDS = '../preprocessing_wordlist/uncertain_words.json'
-REMAINING_NON_COVID_TWEETS = 'non_covid_tweets.csv'
+NON_COVID_TWEETS = 'non_covid_tweets.csv'
 
 results_overview = []
 results_detail = []
-remaining_non_covid_tweets = []
-covid_tweets_id = []
+non_covid_tweets = []
+covid_tweets_ids = []
 
 tweets_of_pol = []
 matching_part_1 = []
 
 wordlist_fuzzy = json.load(codecs.open(PREPROCESSED_WORDLIST_FUZ, 'r', 'utf-8-sig'))
 wordlist = json.load(codecs.open(PREPROCESSED_WORDLIST, 'r', 'utf-8-sig'))
-uncertain_words_list = json.load(codecs.open(UNCERTAIN_WORDS, 'r', 'utf-8-sig'))
+uncertain_words = json.load(codecs.open(UNCERTAIN_WORDS, 'r', 'utf-8-sig'))
 
 
 # Source: https://towardsdatascience.com/basic-tweet-preprocessing-in-python-efd8360d529e
@@ -86,7 +85,7 @@ def check_match(tweets):
         list_uncertain_words = []
         for word in wordlist:
             if word in tweet[1]:
-                if word in uncertain_words_list and len(np.unique(list_uncertain_words)) < 2:
+                if word in uncertain_words and len(np.unique(list_uncertain_words)) < 2:
                     list_uncertain_words.append(word)
                 else:
                     return [word, 'match', tweet[0], tweet[1], tweet[2], tweet[3]]
@@ -110,7 +109,7 @@ def check_match_fuzzy(tweets):
         for word in wordlist_fuzzy:
             for token in tweet[1]:
                 if fuzz.ratio(word, token) > 90:
-                    if token in uncertain_words_list and len(np.unique(list_uncertain_words)) < 2:
+                    if token in uncertain_words and len(np.unique(list_uncertain_words)) < 2:
                         list_uncertain_words.append(token)
                     else:
                         return [word, token, tweet[0], tweet[1], tweet[2], tweet[3]]
@@ -136,7 +135,7 @@ def check_match_regex(tweets):
             for token in tweet[1]:
                 result = re.match(pattern, token)
                 if result is not None:
-                    if token in uncertain_words_list and len(np.unique(list_uncertain_words)) < 2:
+                    if token in uncertain_words and len(np.unique(list_uncertain_words)) < 2:
                         list_uncertain_words.append(token)
                     else:
                         return [pattern, token, tweet[0], tweet[1], tweet[2], tweet[3]]
@@ -171,7 +170,6 @@ with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
             'name': p_name,
             'screen_name': p_screen_name,
             'party': p_party,
-            'total_tweets': len(tweets_of_pol),
             'covid_tweets': len(covid_tweets),
         }
         results_overview.append(p_tweets_stats)
@@ -187,7 +185,7 @@ with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
                 'original tweet': i[3],
                 'tweet': i[4],
             }
-            covid_tweets_id.append(i[5])
+            covid_tweets_ids.append(i[5])
             results_detail.append(p_tweets_stats)
 
         for tweet in non_covid_tweets:
@@ -197,11 +195,11 @@ with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
                 'created at': tweet[0],
                 'tweet': tweet[2],
             }
-            remaining_non_covid_tweets.append(p_remaining_tweets_stats)
+            non_covid_tweets.append(p_remaining_tweets_stats)
 
     keys_results_overview = results_overview[0].keys()
     keys_results_detail = results_detail[0].keys()
-    keys_remaining_tweets = remaining_non_covid_tweets[0].keys()
+    keys_remaining_tweets = non_covid_tweets[0].keys()
 
     with open(RESULTS_FILE_OVERVIEW, 'w', newline='', encoding='utf-8') as outfile:
         dict_writer = csv.DictWriter(outfile, keys_results_overview)
@@ -213,34 +211,23 @@ with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
         dict_writer.writeheader()
         dict_writer.writerows(results_detail)
 
-    with open(REMAINING_NON_COVID_TWEETS, 'w', newline='', encoding='utf-8') as outfile:
+    with open(NON_COVID_TWEETS, 'w', newline='', encoding='utf-8') as outfile:
         dict_writer = csv.DictWriter(outfile, keys_remaining_tweets)
         dict_writer.writeheader()
-        dict_writer.writerows(remaining_non_covid_tweets)
+        dict_writer.writerows(non_covid_tweets)
 
-shutil.rmtree(COVID_TWEETS_SOURCE_FOLDER)
-shutil.rmtree(NON_COVID_TWEETS_SOURCE_FOLDER)
-shutil.copytree(TWEETS_SOURCE_FOLDER, COVID_TWEETS_SOURCE_FOLDER)
-shutil.copytree(TWEETS_SOURCE_FOLDER, NON_COVID_TWEETS_SOURCE_FOLDER)
-
-for filename in os.listdir(COVID_TWEETS_SOURCE_FOLDER):
-    f_path_covid_tweets = os.path.join(COVID_TWEETS_SOURCE_FOLDER, filename)
-    f_path_non_covid_tweets = os.path.join(NON_COVID_TWEETS_SOURCE_FOLDER, filename)
-    covid_tweets = []
-    no_covid_tweets = []
-    if os.path.isfile(f_path_covid_tweets):
-        with open(f_path_covid_tweets, 'r', encoding='utf-8') as infile:
+for filename in os.listdir(TWEETS_SOURCE_FOLDER):
+    f_path = os.path.join(TWEETS_SOURCE_FOLDER, filename)
+    f_path_covid_tweets = os.path.join(COVID_TWEETS_FOLDER, filename)
+    f_path_non_covid_tweets = os.path.join(NON_COVID_TWEETS_FOLDER, filename)
+    if os.path.isfile(f_path):
+        with open(f_path, 'r', encoding='utf-8') as infile:
             all_tweets = [t for t in json.load(infile)]
-            if len(all_tweets) > 0:
-                for tweet in all_tweets:
-                    if tweet['id_'] in covid_tweets_id:
-                        covid_tweets.append(tweet)
-                    else:
-                        no_covid_tweets.append(tweet)
+            covid_tweets = [t for t in all_tweets if t['id_'] in covid_tweets_ids]
+            non_covid_tweets = [t for t in all_tweets if t['id_'] not in covid_tweets_ids]
 
-        with open(f_path_covid_tweets, 'w', encoding='utf-8') as outfile:
-            json.dump(covid_tweets, outfile, ensure_ascii=False)
+    with open(f_path_covid_tweets, 'w', encoding='utf-8') as outfile:
+        json.dump(covid_tweets, outfile, ensure_ascii=False)
 
-    if os.path.isfile(f_path_non_covid_tweets):
-        with open(f_path_non_covid_tweets, 'w', encoding='utf-8') as outfile:
-            json.dump(no_covid_tweets, outfile, ensure_ascii=False)
+    with open(f_path_non_covid_tweets, 'w', encoding='utf-8') as outfile:
+        json.dump(non_covid_tweets, outfile, ensure_ascii=False)
