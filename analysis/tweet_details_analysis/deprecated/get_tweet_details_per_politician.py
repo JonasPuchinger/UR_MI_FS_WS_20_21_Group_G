4679aeec4794c685d9eaa93d00b0185baf8cfd35
Sabine Roth <sabi.roth@t-online.de>
2021-02-11 22:29:41 +0100

    Update lists to csv files

---
+++ analysis/tweet_details_analysis/deprecated/get_tweet_details_per_politician.py
@@ -0,0 +1,132 @@
+import json
+import os
+from operator import itemgetter
+import regex
+import requests
+
+TWEETS_SOURCE_FOLDER = '../formated_data/tweet/'
+POLITICIANS_LIST = '../../assets/all_politicians.json'
+
+def get_politicians_list():
+    new = []
+    with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
+        for p in json.load(infile):
+            new.append(p)
+    return new
+
+politicians_list = get_politicians_list()
+
+def get_politician_info(name):
+    info = dict.fromkeys(['name', 'screen_name','id', 'party'])
+    for p in politicians_list:
+        if p['screen_name'] == name:
+            info['name'] = p['Name']
+            info['screen_name'] = name
+            info['id'] = p['id']
+            info['party'] = p['Partei']
+    return info
+
+def get_domain(url):
+    try:
+        return url[:regex.search(r'(?<!http:|https:|\/)\/|\\', url).span()[0]]
+    except:
+        return url
+
+# Expands shortened URLs to get the actual link
+def expand_url(url):
+    session = requests.Session()
+    try: 
+        resp = session.head(url, allow_redirects=True)
+        return resp.url
+    # If the bit.ly or tinyurl link doesn't exist anymore
+    except: 
+        return url
+
+def get_detailed_info(all_tweets):
+    info = get_politician_info(filename[:-5])
+    return {
+        "name": info['name'],
+        "screen_name": info['screen_name'],
+        "id": info['id'],
+        "party": info['party'],
+        "mentions": get_mentions(all_tweets),
+        "urls": get_links(all_tweets),
+        "hashtags": get_hashtags(all_tweets)
+    }
+
+def sort_list(list_of_dicts):
+    return sorted(list_of_dicts, key=itemgetter('count'), reverse=True)  
+
+def get_mentions(all_tweets):
+    mentions_list_of_dicts = []
+    for tweet in all_tweets:
+        mentions_of_tweet = tweet.get('raw_data').get('entities').get('user_mentions')
+        if len(mentions_of_tweet) > 0:
+            for mention in mentions_of_tweet:
+                screen_name = mention.get('screen_name')
+                found_entry = next((item for item in mentions_list_of_dicts if item['screen_name'] == screen_name), None)
+                if found_entry != None:
+                    found_entry['count'] = found_entry['count'] + 1
+                else:
+                    mention_dict = dict.fromkeys(['screen_name','count'])
+                    mention_dict['screen_name'] = screen_name
+                    mention_dict['count'] = 1
+                    mentions_list_of_dicts.append(mention_dict)
+    sorted_list = sort_list(mentions_list_of_dicts)
+    return sorted_list
+
+def get_links(all_tweets):
+    links_list_of_dicts = []
+    for tweet in all_tweets:
+        links_of_tweet = tweet.get('raw_data').get('entities').get('urls')
+        if len(links_of_tweet) > 0:
+            for link in links_of_tweet:
+                url = link.get('expanded_url')
+                if "bit.ly" in url or "tinyurl.com" in url:
+                    url = expand_url(url)   
+                domain = get_domain(url)
+                found_entry = next((item for item in links_list_of_dicts if item['domain'] == domain), None)
+                if found_entry != None:
+                    found_entry['count'] = found_entry['count'] + 1
+                else:
+                   link_dict = dict.fromkeys(['domain','count'])
+                   link_dict['domain'] = domain
+                   link_dict['count'] = 1
+                   links_list_of_dicts.append(link_dict)
+    sorted_list = sort_list(links_list_of_dicts)
+    return sorted_list                       
+
+def get_hashtags(all_tweets):
+    hashtags_list_of_dicts = []
+    for tweet in all_tweets:
+        hashtags_of_tweet = tweet.get('raw_data').get('entities').get('hashtags')
+        if len(hashtags_of_tweet) > 0:
+            for hashtag in hashtags_of_tweet:
+                text = hashtag.get('text')
+                found_entry = next((item for item in hashtags_list_of_dicts if item['hashtag'] == text), None)
+                if found_entry != None:
+                    found_entry['count'] = found_entry['count'] + 1
+                else:
+                   hashtag_dict = dict.fromkeys(['hashtag','count'])
+                   hashtag_dict['hashtag'] = text
+                   hashtag_dict['count'] = 1
+                   hashtags_list_of_dicts.append(hashtag_dict)
+    sorted_list = sort_list(hashtags_list_of_dicts)
+    return sorted_list   
+
+full_list = []
+
+for entry in politicians_list:
+    filename = entry['screen_name'] + '.json'
+    f_path = os.path.join(TWEETS_SOURCE_FOLDER, filename)
+    if os.path.isfile(f_path):
+        with open(f_path, 'r', encoding='utf-8') as infile:
+            #all_tweets = [t for t in json.load(infile)]
+            all_tweets = [t for t in json.load(infile) if t['raw_data']['user_id_str'] == str(entry['id'])] #just tweets by user
+            full_list.append(get_detailed_info(all_tweets))
+
+def create_file():
+    with open('tweets_by_user/tweet_details_per_politician_all_tweets.json', 'w', encoding='utf-8') as outfile:
+        json.dump(str(full_list), outfile, ensure_ascii=False)
+
+create_file()
--- analysis/tweet_details_analysis/get_tweet_details.py
+++ analysis/tweet_details_analysis/get_tweet_details.py
@@ -1,33 +1,21 @@
 import json
-import os
-import collections
-import regex
+import csv
 import requests
+import regex
+import os
+from datetime import datetime
 
 TWEETS_SOURCE_FOLDER = '../formated_data/tweet/'
 POLITICIANS_LIST = '../../assets/all_politicians.json'
 
-mentions_list = []
-mentions_dict = {}
-domains_list = []
-domains_dict = {}
-link_list = []
-hashtags_list = []
-hashtags_dict = {}
-
 def get_politicians_list():
-    new = []
+    politicians = []
     with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
         for p in json.load(infile):
-            new.append(p)
-    return new
+            politicians.append(p)
+    return politicians
 
 politicians_list = get_politicians_list()
-
-def get_party_of_politician(name):
-    for p in politicians_list:
-        if p['screen_name'] == name:
-            return p['Partei']
 
 def get_domain(url):
     try:
@@ -45,109 +33,128 @@ def expand_url(url):
     except: 
         return url
 
-def get_all_mentions(all_tweets):
-     for tweet in all_tweets:
-        mentions_of_tweet = tweet.get('raw_data').get('entities').get('user_mentions')
-        if len(mentions_of_tweet) > 0:
-            party = get_party_of_politician(filename[:-5])
-            for mention in mentions_of_tweet:
-                name = mention.get('screen_name')
-                mentions_list.append(name)
-                if party in mentions_dict:
-                    mentions_dict[party].append(name)
-                else:
-                    mentions_dict[party] = [name] 
+def get_detail_info(tweet):
+    info = dict.fromkeys(['name', 'screen_name','id', 'party'])
+    for p in politicians_list:
+        if p['screen_name'] == filename[:-5]:
+            info['name'] = p['Name']
+            info['screen_name'] = p['screen_name']
+            info['id'] = p['id']
+            info['party'] = p['Partei']
+    return info
 
-def get_all_links(all_tweets):
-    for tweet in all_tweets:
-        links_of_tweet = tweet.get('raw_data').get('entities').get('urls')
-        if len(links_of_tweet) > 0:
-            party = get_party_of_politician(filename[:-5])
-            for link in links_of_tweet:
-                url = link.get('expanded_url')
-                if "bit.ly" in url or "tinyurl.com" in url:
-                     url = expand_url(url)
-                link_list.append(url) 
-                domain = get_domain(url)
-                domains_list.append(domain)
-                if party in domains_dict:
-                    domains_dict[party].append(domain)
-                else:
-                    domains_dict[party] = [domain]
+def format_date(date):
+    return datetime.strftime(datetime.strptime(date,'%a %b %d %H:%M:%S +0000 %Y'), '%Y-%m-%d') 
 
-def get_all_hashtags(all_tweets):
+def get_hashtags(all_tweets, info_dict):
+    hashtags_list_of_dicts = []
     for tweet in all_tweets:
         hashtags_of_tweet = tweet.get('raw_data').get('entities').get('hashtags')
         if len(hashtags_of_tweet) > 0:
-            party = get_party_of_politician(filename[:-5])
+            date = format_date(tweet.get('raw_data').get('created_at'))
+            tweet_id = tweet.get('raw_data').get('id')
             for hashtag in hashtags_of_tweet:
-                text = hashtag.get('text')
-                hashtags_list.append(text)
-                if party in hashtags_dict:
-                    hashtags_dict[party].append(text)
-                else:
-                    hashtags_dict[party] = [text] 
+                hashtag_dict = dict.fromkeys(['hashtag', 'date', 'tweet_id'])
+                hashtag_dict['hashtag'] = hashtag.get('text')
+                hashtag_dict['date'] = date
+                hashtag_dict['tweet_id'] = tweet_id
+                hashtags_list_of_dicts.append(hashtag_dict)
+    info_dict['hashtags'] = hashtags_list_of_dicts
+    return info_dict
+
+def get_mentions(all_tweets, info_dict):
+    mentions_list_of_dicts = []
+    for tweet in all_tweets:
+        mentions_of_tweet = tweet.get('raw_data').get('entities').get('user_mentions')
+        if len(mentions_of_tweet) > 0:
+            date = format_date(tweet.get('raw_data').get('created_at'))
+            tweet_id = tweet.get('raw_data').get('id')
+            for mention in mentions_of_tweet:
+                mention_dict = dict.fromkeys(['mention', 'date', 'tweet_id'])
+                mention_dict['mention'] = mention.get('screen_name')
+                mention_dict['date'] = date
+                mention_dict['tweet_id'] = tweet_id
+                mentions_list_of_dicts.append(mention_dict)
+    info_dict['mentions'] = mentions_list_of_dicts
+    return info_dict
+
+def get_links(all_tweets, info_dict):
+    links_list_of_dicts = []
+    domains_list_of_dicts = []
+    for tweet in all_tweets:
+        links_of_tweet = tweet.get('raw_data').get('entities').get('urls')
+        if len(links_of_tweet) > 0:
+            date = format_date(tweet.get('raw_data').get('created_at'))
+            tweet_id = tweet.get('raw_data').get('id')
+            for link in links_of_tweet:
+                url = link.get('expanded_url')
+                if "bit.ly" in url or "tinyurl.com" in url:
+                    url = expand_url(url)   
+                link_dict = dict.fromkeys(['link', 'date', 'tweet_id'])
+                link_dict['link'] = url
+                link_dict['date'] = date
+                link_dict['tweet_id'] = tweet_id
+                links_list_of_dicts.append(link_dict)   
+                domain = get_domain(url)
+                domain_dict = dict.fromkeys(['domain', 'date', 'tweet_id'])
+                domain_dict['domain'] = domain
+                domain_dict['date'] = date
+                domain_dict['tweet_id'] = tweet_id
+                domains_list_of_dicts.append(domain_dict)  
+    all_links_dict = info_dict.copy()
+    all_links_dict['links'] = links_list_of_dicts
+    all_domains_dict = info_dict.copy()
+    all_domains_dict['domains'] = domains_list_of_dicts   
+    return all_links_dict, all_domains_dict                   
+
+hashtags_tweets_by_user = []
+hashtags_all_tweets = []
+mentions_tweets_by_user = []
+mentions_all_tweets = []
+links_tweets_by_user = []
+links_all_tweets = []
+domains_tweet_by_user = []
+domains_all_tweets = []
 
 for entry in politicians_list:
     filename = entry['screen_name'] + '.json'
     f_path = os.path.join(TWEETS_SOURCE_FOLDER, filename)
     if os.path.isfile(f_path):
         with open(f_path, 'r', encoding='utf-8') as infile:
-            #all_tweets = [t for t in json.load(infile)]
-            all_tweets = [t for t in json.load(infile) if t['raw_data']['user_id_str'] == str(entry['id'])] #just tweets by user
-            get_all_mentions(all_tweets)
-            get_all_links(all_tweets)
-            get_all_hashtags(all_tweets)
+            f_content = json.load(infile)
+            if f_content != []:
+                all_tweets = [t for t in f_content]
+                detail_info = get_detail_info(all_tweets[0])
+                hashtags_all_tweets.append(get_hashtags(all_tweets, detail_info.copy()))
+                mentions_all_tweets.append(get_mentions(all_tweets, detail_info.copy()))
+                links, domains = get_links(all_tweets, detail_info.copy())
+                links_all_tweets.append(links)
+                domains_all_tweets.append(domains)
+                
+                #just tweets by user himself
+                all_tweets = [t for t in all_tweets if t['raw_data']['user_id_str'] == str(entry['id'])] 
+                hashtags_tweets_by_user.append(get_hashtags(all_tweets, detail_info.copy()))
+                mentions_tweets_by_user.append(get_mentions(all_tweets, detail_info.copy()))
+                links, domains = get_links(all_tweets, detail_info.copy())
+                links_tweets_by_user.append(links)
+                domains_tweet_by_user.append(domains)
 
-def create_mentions_count_file():
-    count_list = collections.Counter(mentions_list).most_common()
-    with open('tweets_by_user/mentions_count_all_tweets.json', 'w', encoding='utf-8') as outfile:
-        json.dump(str(count_list), outfile, ensure_ascii=False)
-
-def create_mentions_party_file():
-    for key, value in mentions_dict.items():
-        count_list = collections.Counter(value).most_common()
-        mentions_dict[key] = count_list
-
-    with open('tweets_by_user/mentions_party_all_tweets.json', 'w', encoding='utf-8') as outfile:
-        json.dump(str(mentions_dict), outfile, ensure_ascii=False)
-
-create_mentions_count_file()
-create_mentions_party_file()
-
-def create_domains_count_file():
-    count_list = collections.Counter(domains_list).most_common()
-    with open('tweets_by_user/domains_count_all_tweets.json', 'w', encoding='utf-8') as outfile:
-        json.dump(str(count_list), outfile, ensure_ascii=False)
-
-def create_domains_party_file():
-    for key, value in domains_dict.items():
-        count_list = collections.Counter(value).most_common()
-        domains_dict[key] = count_list
-    with open('tweets_by_user/domains_party_all_tweets.json', 'w', encoding='utf-8') as outfile:
-        json.dump(str(domains_dict), outfile, ensure_ascii=False)
-
-def create_links_count_file():
-    count_list = collections.Counter(link_list).most_common()
-    with open('tweets_by_user/links_count_all_tweets.json', 'w', encoding='utf-8') as outfile:
-        json.dump(str(count_list), outfile, ensure_ascii=False)
-
-create_domains_count_file()
-create_domains_party_file()
-create_links_count_file()
-
-def create_hashtags_count_file():
-    count_list = collections.Counter(hashtags_list).most_common()
-    with open('tweets_by_user/hashtags_count_all_tweets.json', 'w', encoding='utf-8') as outfile:
-        json.dump(str(count_list), outfile, ensure_ascii=False)
-
-def create_hashtags_party_file():
-    for key, value in hashtags_dict.items():
-        count_list = collections.Counter(value).most_common()
-        hashtags_dict[key] = count_list
-    with open('tweets_by_user/hashtags_party_all_tweets.json', 'w', encoding='utf-8') as outfile:
-        json.dump(str(hashtags_dict), outfile, ensure_ascii=False)
+def create_file(data, file):
+    keys = data[0].keys()
+    with open(file, 'w', newline='', encoding='utf-8') as outfile:
+        dict_writer = csv.DictWriter(outfile, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
 
 
-create_hashtags_count_file()
-create_hashtags_party_file()
+def create_files():
+    create_file(hashtags_tweets_by_user, 'tweets_by_user/hashtags_tweets_by_user.csv')
+    create_file(mentions_tweets_by_user, 'tweets_by_user/mentions_tweets_by_user.csv')
+    create_file(links_tweets_by_user, 'tweets_by_user/links_tweets_by_user.csv')
+    create_file(domains_tweet_by_user, 'tweets_by_user/domains_tweets_by_user.csv')
+    create_file(hashtags_all_tweets, 'all_tweets/hashtags_all_tweets.csv')
+    create_file(mentions_all_tweets, 'all_tweets/mentions_all_tweets.csv')
+    create_file(links_all_tweets, 'all_tweets/links_all_tweets.csv')
+    create_file(domains_all_tweets, 'all_tweets/domains_all_tweets.csv')
+
+create_files()
