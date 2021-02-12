97f6e2c87be71b96ae70759f4a822aa55d9a2f55
TheresaStrohmeier <strohmeier.theresa@web.de>
2021-02-12 02:03:30 +0100

    Change preprocessing of tweets and wordlist, update data

--- analysis/covid_tweets_analysis/get_covid_tweets.py
+++ analysis/covid_tweets_analysis/get_covid_tweets.py
@@ -3,11 +3,10 @@ import numpy as np
 import csv
 import json
 import re
-import preprocessor as pre
-from nltk.corpus import stopwords
 import codecs
 from fuzzywuzzy import fuzz
-from nltk.tokenize import TweetTokenizer
+from analysis.clean_data import clean_for_filtering
+
 
 POLITICIANS_LIST = '../../assets/all_politicians.json'
 TWEETS_SOURCE_FOLDER = '../formated_data/tweet/'
@@ -15,8 +14,7 @@ COVID_TWEETS_FOLDER = '../filtered_data/covid_tweets/'
 NON_COVID_TWEETS_FOLDER = '../filtered_data/non_covid_tweets/'
 RESULTS_FILE_OVERVIEW = 'covid_tweets_overview.csv'
 RESULTS_FILE_DETAIL = 'covid_tweets_detail.csv'
-PREPROCESSED_WORDLIST_FUZ = '../preprocessing_wordlist/wordlist_fuzzy.json'
-PREPROCESSED_WORDLIST = '../preprocessing_wordlist/wordlist_matching.json'
+PREPROCESSED_WORDLIST = '../preprocessing_wordlist/preprocessed_wordlist.json'
 UNCERTAIN_WORDS = '../preprocessing_wordlist/uncertain_words.json'
 NON_COVID_TWEETS = 'non_covid_tweets.csv'
 
@@ -25,38 +23,8 @@ results_detail = []
 non_covid_tweets = []
 covid_tweets_ids = []
 
-wordlist_fuzzy = json.load(codecs.open(PREPROCESSED_WORDLIST_FUZ, 'r', 'utf-8-sig'))
 wordlist = json.load(codecs.open(PREPROCESSED_WORDLIST, 'r', 'utf-8-sig'))
 uncertain_words = json.load(codecs.open(UNCERTAIN_WORDS, 'r', 'utf-8-sig'))
-
-
-# Source: https://towardsdatascience.com/basic-tweet-preprocessing-in-python-efd8360d529e
-def preprocessing_tweet(original_tweet):
-    tokenizer = TweetTokenizer()
-    # Text-Cleaning (URLs, Mentions, Reserved words, Smileys, Numbers)
-    pre.set_options(pre.OPT.URL, pre.OPT.MENTION, pre.OPT.RESERVED, pre.OPT.SMILEY, pre.OPT.NUMBER)
-    preprocessed_tweet = pre.clean(original_tweet)
-    # Removal of digits
-    preprocessed_tweet = re.sub('\d+', '', preprocessed_tweet)
-    # lowercase
-    preprocessed_tweet = preprocessed_tweet.lower()
-
-    def remove_punctuation(words):
-        new_words = []
-        for word in words:
-            new_word = re.sub(r'[^\w\s]', '', (word))
-            if new_word != '':
-                new_words.append(new_word)
-        return new_words
-
-    # Tokenization
-    preprocessed_words = tokenizer.tokenize(preprocessed_tweet)
-    # Remove Punctuations
-    preprocessed_words = remove_punctuation(preprocessed_words)
-    # Remove stop words
-    stop_words = set(stopwords.words('german'))
-    preprocessed_words = [word for word in preprocessed_words if not word in stop_words]
-    return preprocessed_words
 
 
 def get_all_tweets_by_politician(screen_name, user_id):
@@ -68,7 +36,7 @@ def get_all_tweets_by_politician(screen_name, user_id):
             tweets_by_user = [t for t in all_tweets if t['raw_data']['user_id_str'] == str(user_id)]
             result = []
             for t in tweets_by_user:
-                result.append([t, preprocessing_tweet(t['raw_data']['full_text'])])
+                result.append([t, clean_for_filtering(t['raw_data']['full_text'])])
             return result
     return ""
 
@@ -85,7 +53,7 @@ def get_quote_tweets(screen_name, remaining_tweets):
             quote_tweets = [t for t in all_tweets if t['id_'] in quote_tweets_ids]
             result = []
             for t in quote_tweets:
-                result.append([t['id_'], preprocessing_tweet(t['raw_data']['full_text'])])
+                result.append([t['id_'], clean_for_filtering(t['raw_data']['full_text'])])
             return result
     return ""
 
@@ -96,6 +64,7 @@ def check_match(tweets):
 
     def match_wordlist_and_tweet(tweet):
         list_uncertain_words = []
+
         for word in wordlist:
             if word in tweet[1]:
                 if word in uncertain_words and len(np.unique(list_uncertain_words)) < 2:
@@ -145,7 +114,7 @@ def check_match_fuzzy(tweets):
 
     def match_fuzzy(tweet):
         list_uncertain_words = []
-        for word in wordlist_fuzzy:
+        for word in wordlist:
             for token in tweet[1]:
                 if fuzz.ratio(word, token) > 90:
                     if token in uncertain_words and len(np.unique(list_uncertain_words)) < 2:
@@ -217,8 +186,9 @@ with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
         p_user_id = p['id']
         p_screen_name = p['screen_name']
         p_party = p['Partei']
+
         all_tweets_by_politician = get_all_tweets_by_politician(screen_name=p_screen_name, user_id=p_user_id)
-        print(p_name)
+
         results_matching = match_tweets(all_tweets_by_politician)
 
         remaining_tweets = results_matching[1]
@@ -265,6 +235,7 @@ with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
         dict_writer = csv.DictWriter(outfile, keys_remaining_tweets)
         dict_writer.writeheader()
         dict_writer.writerows(non_covid_tweets)
+
 
 for filename in os.listdir(TWEETS_SOURCE_FOLDER):
     f_path = os.path.join(TWEETS_SOURCE_FOLDER, filename)
--- analysis/preprocessing_wordlist/preprocess_wordlists.py
+++ analysis/preprocessing_wordlist/preprocess_wordlists.py
@@ -1,43 +1,16 @@
 import json
 import codecs
-import preprocessor as pre
-import re
+from analysis.clean_data import clean_for_filtering
 
-# Preprocessing wordlists
+# Preprocessing wordlist
 
-WORDLIST_FUZZY = 'wordlist_fuzzy.json'
-WORDLIST_MATCHING = 'wordlist_matching.json'
+PREPROCESSED_WORDLIST = 'preprocessed_wordlist.json'
 WORDLIST = '../../assets/wordlist.json'
 
 wordlist = json.load(codecs.open(WORDLIST, 'r', 'utf-8-sig'))
-pre.set_options(pre.OPT.NUMBER)
 
+preprocessed_wordlist = [clean_for_filtering(word)[0] for word in wordlist]
 
-def preprocess_wordlist(wordlist):
-    preprocessed_words = []
-    for word in wordlist:
-        lower_word = word.lower()
-        # Cleaning (Numbers)
-        word = pre.clean(lower_word)
-        # Removal of digits
-        word = re.sub('\d+', '', word)
-        # Remove Punctuations
-        word = re.sub(r'[^\w\s]', '', word)
-        preprocessed_words.append(word)
-    # Remove duplicates
-    preprocessed_wordlist = set(preprocessed_words)
-    return list(preprocessed_wordlist)
+with open(PREPROCESSED_WORDLIST, 'w', encoding='utf-8') as f:
+    json.dump(preprocessed_wordlist, f, ensure_ascii=False)
 
-
-remove_words = ["R0", "#n95"]
-updated_wordlist = [word for word in wordlist if word not in remove_words]
-
-wordlist_matching = preprocess_wordlist(updated_wordlist)
-remove_words_fuz = ["pcr", "rzahl", "rwert", "rwerte"]
-wordlist_fuzzy = [word for word in wordlist_matching if word not in remove_words_fuz]
-
-with open(WORDLIST_MATCHING, 'w', encoding='utf-8') as f:
-    json.dump(wordlist_matching, f, ensure_ascii=False)
-
-with open(WORDLIST_FUZZY, 'w', encoding='utf-8') as f:
-    json.dump(wordlist_fuzzy, f, ensure_ascii=False)
