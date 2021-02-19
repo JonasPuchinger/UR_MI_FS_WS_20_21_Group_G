411412dcf6c03782be2bd0d268a0fe493370a3a2
Sabine Roth <sabi.roth@t-online.de>
2021-02-19 17:49:08 +0100

    Quarter content analysis

--- analysis/content_analysis/content_analysis.py
+++ analysis/content_analysis/content_analysis.py
@@ -13,18 +13,8 @@ import time
 TWEETS_SOURCE_FOLDER = '../formated_data/tweet/'
 POLITICIANS_LIST = '../../assets/all_politicians.json'
 
-start_end_date = [['01/02/2020', '29/02/2020']]
-
-""" ,['03/1/2020', '03/31/2020'],
-['04/1/2020', '04/30/2020'],
-['05/1/2020', '05/31/2020'],
-['06/1/2020', '06/30/2020'],
-['07/1/2020', '07/31/2020'],
-['08/1/2020', '08/31/2020'],
-['09/1/2020', '09/30/2020'],
-['10/1/2020', '10/31/2020'],
-['11/1/2020', '11/30/2020'],
-['12/1/2020', '12/31/2020'], """
+#quarters = [['01/01/2020', '31/03/2020'], ['01/04/2020', '30/06/2020'], ['01/07/2020', '30/09/2020'], ['01/10/2020', '31/12/2020']]
+quarters = [['01/10/2020', '31/12/2020']]
 
 def get_politicians_list():
         with open(POLITICIANS_LIST, 'r', encoding='utf-8') as infile:
@@ -35,10 +25,12 @@ def format_date(current_date):
     return datetime.strftime(datetime.strptime(current_date,'%a %b %d %H:%M:%S +0000 %Y'), '%d/%m/%Y') 
 
 def check_date(current_date):
-    return True if time.strptime(current_date, '%d/%m/%Y') >= time.strptime(date[0], '%d/%m/%Y') and time.strptime(current_date, '%d/%m/%Y') <= time.strptime(date[1], '%d/%m/%Y') else False
+    return True if time.strptime(current_date, '%d/%m/%Y') >= time.strptime(quarter[0], '%d/%m/%Y') and time.strptime(current_date, '%d/%m/%Y') <= time.strptime(quarter[1], '%d/%m/%Y') else False
 
-for date in start_end_date:
-    path = os.getcwd() + "\\" + 'february'
+for quarter in quarters:
+    quarter_value = quarters.index(quarter) + 4
+    print(quarter_value)
+    path = os.getcwd() + "\\" + 'Quarter_' + str(quarter_value)
     Path(path).mkdir(parents=True, exist_ok=True)
     path = path + "\\"
     text_data = []
@@ -61,7 +53,7 @@ for date in start_end_date:
         # get LDA model and the topic
         if __name__ == '__main__':
             num_topics = 5
-            lda_model = gensim.models.LdaMulticore(corpus=corpus, num_topics = num_topics, id2word=id2word, passes=10)
+            lda_model = gensim.models.LdaMulticore(corpus=corpus, num_topics = num_topics, id2word=id2word, passes=2)
             lda_model.save(path + 'model5.gensim')
             topics = lda_model.print_topics(num_words=6)
             with open(path + "Topics.txt", "w") as text_file:
