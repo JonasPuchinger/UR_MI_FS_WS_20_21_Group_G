75cbc235a4409500e3ab0187a7cf77dc77c16a77
Sabine Roth <sabi.roth@t-online.de>
2021-02-21 15:29:04 +0100

    Update clean_data and visulization for qualitative content analysis

--- analysis/clean_data.py
+++ analysis/clean_data.py
@@ -6,26 +6,30 @@ from nltk.corpus import wordnet as wn
 import re
 from textblob_de import TextBlobDE as TextBlob
 
-nlp = spacy.load("de_core_news_sm")
+nlp = spacy.load("de_core_news_md")
 
 try:
     de_stop = set(nltk.corpus.stopwords.words('german'))
+    eng_stop = set(nltk.corpus.stopwords.words('english'))
 except:
     # download wordnet and stopwords if necessary
     nltk.download('wordnet')
     nltk.download('stopwords')
     de_stop = set(nltk.corpus.stopwords.words('german'))
+    eng_stop = set(nltk.corpus.stopwords.words('english'))
 
 def clean_for_lda(text):
     tokens = nlp(text)
     tokens = [token for token in tokens if remove_whitespaces(token)]
-    tokens = [get_lemma(token) for token in tokens]
-    tokens = [to_lower(token) for token in tokens]
+    tokens = [token for token in tokens if remove_punctuation_and_smileys(token)]
     tokens = [token for token in tokens if remove_stopwords(token)]
     tokens = [token for token in tokens if remove_url(token)]
     tokens = [token for token in tokens if remove_mention(token)]
-    tokens = [token for token in tokens if remove_punctuation_and_smileys(token)]
+    tokens = [get_lemma(token) for token in tokens]
+    tokens = [to_lower(token) for token in tokens]
+    tokens = [token for token in tokens if str(token) != 'amp']
     tokens = [token for token in tokens if get_nouns(token)]
+    tokens = [token for token in tokens if len(token) > 2]
     return [str(token) for token in tokens]
 
 def clean_for_filtering(text):
@@ -39,7 +43,7 @@ def clean_for_filtering(text):
     return [str(token) for token in tokens]
 
 def remove_stopwords(token):
-    return True if str(token) not in de_stop else False
+    return True if str(token) not in de_stop and str(token) not in eng_stop else False
 
 def remove_url(token):
     return False if token.like_url else True
